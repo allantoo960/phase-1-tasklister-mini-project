f4c172e0ee99512f0416032c70cb75250e5a4cc3
allantoo960 <allankim540@gmail.com>
2025-03-16 22:01:40 +0300

    tasklister project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+})
+const taskListArray=[];
+const submitTask=document.getElementById('sumit task')
+submitTask.addEventListener=document.getElementById("new task description")
+
+taskListArray.push(newTaskDescription)
+console.log(taskListArray)
+
+const toDoList=document.getElementById("tasks")
+
+taskListArray.forEach((task)=>{
+  const li=document.createElement("li")
+  li.innerText=task;
+  toDoList.apprendChild(li);
+  console.log(task)
 });
+newTaskDescription.value=taskListArray;
+
+form.addEventListener("submit",funtion(s)) (
+  s.preventDefault() 
+)
